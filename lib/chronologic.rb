b3bc3b86ecf253b963f4ac34d9dfa40972cc1866
Adam Keys <adam@therealadam.com>
2010-10-14 16:52:54 -0500

    Add connection (for the service) and client attributes.

--- lib/chronologic.rb
+++ lib/chronologic.rb
@@ -2,3 +2,14 @@ require 'chronologic/client'
 require 'chronologic/connection'
 require 'chronologic/server'
 require 'chronologic/version'
+
+require "active_support/core_ext/module"
+
+module Chronologic
+
+  VERSION = "0.1.0"
+
+  mattr_accessor :connection
+  mattr_accessor :client
+
+end

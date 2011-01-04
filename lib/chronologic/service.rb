4e506c260dee94c5c4e7f6cb103d30909b1b223f
Adam Keys <adam@therealadam.com>
2011-01-04 16:57:48 -0600

    Screw it, we'll debug it live.

--- lib/chronologic/service.rb
+++ lib/chronologic/service.rb
@@ -66,6 +66,9 @@ class Chronologic::Service < Sinatra::Base
     end
 
     def event
+      logger.error "Chronologic event:"
+      logger.error "  #{params.inspect}"
+
       timestamp = Time.parse(params["timestamp"])
       Chronologic::Event.new(params.update("timestamp" => timestamp))
     end

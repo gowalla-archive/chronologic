7c185c65d04b0310f8a653d417321903518e8de4
Adam Keys <adam@therealadam.com>
2011-01-14 11:40:56 -0600

    Release 0.5.2

--- lib/chronologic.rb
+++ lib/chronologic.rb
@@ -9,7 +9,7 @@ module Chronologic
     Chronologic::Schema
   end
 
-  VERSION = '0.5.1'
+  VERSION = '0.5.2'
 
   autoload :Schema, "chronologic/schema"
   autoload :Protocol, "chronologic/protocol"

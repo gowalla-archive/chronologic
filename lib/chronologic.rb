ddd7dd67dc6e0964577ec754a3ee84fbe29e0b86
Adam Keys <adam@therealadam.com>
2011-01-28 16:26:53 -0600

    Release 0.7.0

--- lib/chronologic.rb
+++ lib/chronologic.rb
@@ -9,7 +9,7 @@ module Chronologic
     Chronologic::Schema
   end
 
-  VERSION = '0.6.1'
+  VERSION = '0.7.0'
 
   autoload :Schema, "chronologic/schema"
   autoload :Protocol, "chronologic/protocol"

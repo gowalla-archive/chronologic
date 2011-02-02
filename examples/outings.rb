d5cbce742a3bb124c84f47c39ecce61246e76707
Adam Keys <adam@therealadam.com>
2011-02-02 13:53:33 -0600

    Event deletion example.

---
+++ examples/outings.rb
@@ -0,0 +1,49 @@
+require "chronologic"
+
+cl = Chronologic::Client.new("http://localhost:9292")
+
+# Create ten events, push to ten timelines, unpublish them, and publish a new
+# event in the middle of the sequence
+
+user = "outing_user_0"
+users = 10.times.map { |i| "outing_user_#{i + 1}" }
+# users.each { |u| cl.subscribe("outing_user_#{u}_feed", user) }
+users.each { |u| cl.subscribe(user, "#{u}_feed") }
+
+events = 10.times.map do |i|
+  event = Chronologic::Event.new.tap do |e|
+    e.key = "checkin_#{i}"
+    e.timestamp = Time.now + i
+    e.data = {"message" => "This is checkin #{i}"}
+    e.timelines = [user]
+  end
+  [event, cl.publish(event).split('/').last]
+end
+
+# Now we're going to create an outing from event[4] and event[5].
+cl.unpublish(events[4].first.key, events[4].last)
+cl.unpublish(events[5].first.key, events[5].last)
+
+outing = Chronologic::Event.new.tap do |e|
+  e.key = "outing_1"
+  e.timestamp = events[4].first.timestamp 
+  e.data = {"type" => "flight", "note" => "This should appear after checkin 3"}
+  e.timelines = [user]
+end
+cl.publish(outing)
+
+
+events[4].first.timelines = ["outing_1"]
+events[5].first.timelines = ["outing_1"]
+cl.publish(events[4].first)
+cl.publish(events[5].first)
+
+cl.timeline(user)["items"].each do |event|
+  puts "#{event.key} - #{event['data'].inspect} - #{event.timestamp}"
+end
+
+puts "---"
+
+cl.timeline("outing_user_1_feed")["items"].each do |event|
+  puts "#{event.key} - #{event['data'].inspect} - #{event.timestamp}"
+end

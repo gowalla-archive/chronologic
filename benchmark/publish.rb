1ed8ffafea694dc0dd6cd1b1c0be6bd3ae4f543e
Adam Keys <adam@therealadam.com>
2011-03-01 16:16:08 -0600

    Add a benchmark.

---
+++ benchmark/publish.rb
@@ -0,0 +1,51 @@
+# $ ruby -rubygems -Ilib benchmark/publish.rb
+
+require "ruby-prof"
+require "benchmark"
+require "chronologic"
+
+def create_friendship(timeline, subscriber)
+  Chronologic::Protocol.subscribe(timeline, subscriber)
+end
+
+def create_event(key, timeline)
+  ev = Chronologic::Event.new
+  ev.key = key
+  ev.timestamp = Time.now
+  ev.data = {"test" => true}
+  ev.objects = {}
+  ev.timelines = [timeline]
+
+  RubyProf.resume do
+    Chronologic::Protocol.publish(ev)
+  end
+end
+
+def dump_results(data)
+  flat = File.expand_path(File.dirname(__FILE__) + "publish_flat.txt")
+  graph = File.expand_path(File.dirname(__FILE__) + "publish_graph.html")
+
+  RubyProf::FlatPrinter.new(data).print(File.open(flat, "w"), 1)
+  RubyProf::GraphHtmlPrinter.new(data).print(File.open(graph, "w"))
+end
+
+friends = 50
+events = 1000
+timeline = "cl_timeline"
+Chronologic.connection = Cassandra.new("Chronologic")
+
+if __FILE__ == $PROGRAM_NAME
+  result = Benchmark.measure do
+    friends.times { |i| create_friendship("cl_subscriber_#{i}", timeline) }
+  end
+  puts "%.5fms/subscribe" % [friends/result.total]
+
+  result = Benchmark.measure do
+    events.times { |i| create_event("cl_event_#{i}", timeline) }
+  end
+  puts "%.5fms/publish" % [events/result.total]
+
+  data = RubyProf.stop
+  dump_results(data)
+
+end

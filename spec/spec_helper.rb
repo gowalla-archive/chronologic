8f278e14f4065f8ea1958410acf596c593c112f5
Adam Keys <adam@therealadam.com>
2011-06-14 09:50:11 -0500

    Port Event tests to RSpec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,25 @@
 require 'chronologic'
+
+RSpec.configure do |config|
+  
+  def simple_event
+    Chronologic::Event.new.tap do |event|
+      event.key = "checkin_1"
+      event.timestamp = Time.now.utc
+      event.data = {"type" => "checkin", "message" => "I'm here!"}
+      event.objects = {"user" => "user_1", "spot" => "spot_1"}
+      event.timelines = ["user_1", "spot_1"]
+    end
+  end
+
+  def nested_event
+    Chronologic::Event.new.tap do |event|
+      event.key = "comment_1"
+      event.timestamp = Time.now.utc
+      event.data = {"type" => "comment", "message" => "Me too!", "parent" => "checkin_1"}
+      event.objects = {"user" => "user_2", "checkin" => "checkin_1"}
+      event.timelines = ["checkin_1"]
+    end
+  end
+
+end

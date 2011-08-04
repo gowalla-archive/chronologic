535c9a062c3546e24fc97c664232b96c6d0102fa
Adam Keys <adam@therealadam.com>
2011-08-04 08:31:38 -0500

    Start new examples.

---
+++ examples/01_hello_cl.rb
@@ -0,0 +1,26 @@
+require 'boilerplate'
+
+connection = Chronologic::Client::Connection.new(
+  'http://localhost:9292'
+)
+
+# TODO: implement timeline truncation to make for nice examples
+# connection.truncate('home')
+
+event = Chronologic::Event.new
+event.key = "story_1"
+event.timestamp = Time.now.utc
+event.data = {
+  "headline" => "First ever post in Chronologic!",
+  "lede" => "A monumental occasion for housecats everywhere.",
+  "body" => "There is currently a cat perched on my arm. This is normal, carry on!"
+}
+event.timelines = ['home']
+
+connection.publish(event)
+
+feed = connection.timeline('home')
+
+puts "We found #{feed['count']} events."
+puts "That event looks just like this:"
+pp feed['items'].first

839e62088ba57db2487c3fd0feed30937049e0ed
Adam Keys <adam@therealadam.com>
2011-08-17 16:33:31 -0500

    More functional examples.

--- spec/functional/api_spec.rb
+++ spec/functional/api_spec.rb
@@ -6,10 +6,39 @@ describe "The Chronologic API" do
 
   context "GET /event/[event_key]" do
 
-    it "returns an event"
+    it "returns an event" do
+      event = simple_event
+      url = connection.publish(simple_event)
+
+      connection.fetch(url).key.should eq(event.key)
+    end
 
     it "returns 404 if the event isn't present" do
       expect { connection.fetch('/event/thingy_123') }.to raise_exception(Chronologic::NotFound)
+    end
+
+  end
+
+  context "PUT /event/[event_key]" do
+
+    it "updates an existing event" do
+      event = simple_event
+      url = connection.publish(simple_event)
+
+      event.data["brand-new"] = "totally fresh!"
+      connection.update(event)
+
+      connection.fetch(url).data["brand-new"].should eq("totally fresh!")
+    end
+
+    it "reprocesses timelines if update_timeline is set" do
+      event = simple_event
+      url = connection.publish(simple_event)
+
+      event.timelines << 'another_timeline'
+      connection.update(event, true)
+
+      connection.timeline('another_timeline')["items"].first.key.should eq(event.key)
     end
 
   end

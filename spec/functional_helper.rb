04a01a11e5f9b2c02253d0ad1319a90f40672f99
Adam Keys <adam@therealadam.com>
2011-08-17 15:55:17 -0500

    Extract spec helper methods for use in unit and functional examples.

--- spec/functional_helper.rb
+++ spec/functional_helper.rb
@@ -1,5 +1,7 @@
 require 'chronologic'
 require 'rack/test'
+require 'helpers'
 
 RSpec.configure do |config|
+  config.include ChronologicHelpers
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,63 +1,7 @@
 require 'chronologic'
 require 'webmock/rspec'
 require 'cassandra/mock'
-
-module ChronologicHelpers
-
-  def simple_event
-    Chronologic::Event.new.tap do |event|
-      event.key = "checkin_1"
-      event.timestamp = Time.now.utc
-      event.data = {"type" => "checkin", "message" => "I'm here!"}
-      event.objects = {"user" => "user_1", "spot" => "spot_1"}
-      event.timelines = ["user_1", "spot_1"]
-    end
-  end
-
-  def nested_event
-    Chronologic::Event.new.tap do |event|
-      event.key = "comment_1"
-      event.timestamp = Time.now.utc
-      event.data = {"type" => "comment", "message" => "Me too!", "parent" => "checkin_1"}
-      event.objects = {"user" => "user_2"}
-      event.timelines = ["checkin_1"]
-    end
-  end
-
-  def populate_timeline
-    jp = {"name" => "Juan Pelota's"}
-    protocol.record("spot_1", jp)
-
-    uuids = []
-    %w{sco jc am pb mt rm ak ad rs bf}.each_with_index do |u, i|
-      record = {"name" => u}
-      key = "user_#{i}"
-      protocol.record(key, record)
-
-      protocol.subscribe("user_1_home", "user_#{i}")
-
-      event = simple_event
-      event.key = "checkin_#{i}"
-      event.objects["user"] = key
-      event.timelines = [key, "spot_1"]
-      uuids << protocol.publish(event)
-    end
-
-    return uuids
-  end
-
-  # Cassandra#truncate isn't reliable against cassandra-0.7.4, but
-  # this gets the job done. It's a hack, so look for a better way to do this
-  # every now and then
-  def clean_up_keyspace!(conn)
-    conn.schema.cf_defs.each do |cf|
-      conn.send(:each_key, cf.name) do |k|
-        conn.remove(cf.name, k)
-      end
-    end
-  end
-
-end
+require 'helpers'
 
 RSpec.configure do |config|
   config.include(ChronologicHelpers)
